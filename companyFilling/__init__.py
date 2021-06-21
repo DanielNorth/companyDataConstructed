80fa9558a57831ec7a7e450510c069a7d563b7ec
Daniel <danielsplash10@gmail.com>
2021-06-21 18:14:19 -0400

    add the admin view feature

--- companyFilling/__init__.py
+++ companyFilling/__init__.py
@@ -4,13 +4,14 @@ from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 from companyFilling.config import Config
 from flask_mail import Mail
-
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.login_view = 'users.login/'
 login_manager.login_message_category = 'info'
 mail = Mail()
+migrate = Migrate()
 
 
 def create_app():
@@ -23,8 +24,9 @@ def create_app():
     db.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
+    migrate.init_app(app=app, db=db)
 
-    from companyFilling.model import User, Company, Nar1data
+    from companyFilling.model import User, Company, ShareHolderStake, ShareCapital, Director
     with app.app_context():
        db.create_all()
 
@@ -47,13 +49,40 @@ def create_app():
     from companyFilling.shareHolder.routes import shareHolder
     app.register_blueprint(shareHolder, url_prefix='/share_holder')
 
+    from companyFilling.fileNar1.routes import fileNar1
+    app.register_blueprint(fileNar1, url_prefix='/Nar1')
+
+    from companyFilling.allDocument.routes import allDocuments
+    app.register_blueprint(allDocuments, url_prefix="/documents")
+
+    from companyFilling.incorprate_new_company.routes import incorporate_new_company
+    app.register_blueprint(incorporate_new_company, url_prefix="/incorporate_new_company")
+
+    from flask_login import current_user
 
     from flask_admin.contrib.sqla import ModelView
-    from flask_admin import Admin
-    admins = Admin(app)
-    admins.add_view(ModelView(User, db.session))
-    admins.add_view((ModelView(Company, db.session)))
-    admins.add_view(ModelView(Nar1data, db.session))
+    class CompanyModelView(ModelView):
+        can_delete = True  # disable model deletion
+        page_size = 50  # the number of entries to display on the list view
 
+        def is_accessible(self):
+            return int(current_user.id) == 3
+
+        
+    login_manager.login_view = 'admin.login'
+    #from flask_admin.contrib.sqla import ModelView
+    from flask_admin import Admin
+    admins = Admin(app, name="Dashboard", url="/sitting_by_the_riverside")
+
+
+
+    admins.add_view(CompanyModelView(User, db.session))
+    admins.add_view(CompanyModelView(Company, db.session))
+    admins.add_view(CompanyModelView(ShareHolderStake, db.session))
+    admins.add_view(CompanyModelView(ShareCapital, db.session))
+    admins.add_view(CompanyModelView(Director, db.session))
+
+    # from companyFilling.admin import admin
+    # app.register_blueprint(admin, url_prefix='/admin')
 
     return app
