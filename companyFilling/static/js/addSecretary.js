31e6ee524fbaec9d0bf03c4be57fd97ea4cd9d9e
Daniel <danielsplash10@gmail.com>
2021-07-08 16:50:36 -0400

    update on static

---
+++ companyFilling/static/js/addSecretary.js
@@ -0,0 +1,14 @@
+  function hideA()
+  {
+
+      document.getElementById("A").style.visibility="hidden";
+      document.getElementById("B").style.visibility="visible";
+
+  }
+
+  function hideB()
+  {
+      document.getElementById("B").style.visibility="hidden";
+      document.getElementById("A").style.visibility="visible";
+
+  }
