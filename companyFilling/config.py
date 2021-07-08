05c7dd4f1837ebd351eb684219bbcf17545cc721
Daniel <danielsplash10@gmail.com>
2021-07-08 16:52:13 -0400

    update on config.py

--- companyFilling/config.py
+++ companyFilling/config.py
@@ -2,27 +2,21 @@ import os
 
 
 class Config:
-    #SECRET_KEY = os.environ.get('SECRET_KEY')  # WvGFY145LzM2lu7dP70Xb15kdh8I68lL
-    SECRET_KEY = "WvGFY145LzM2lu7dP70Xb15kdh8I68lL"
-    #SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')  # change to other sql database later
+    SECRET_KEY = os.environ.get("SECRET_KEY")
 
-    import urllib.parse
-
-    servername = "hspector"
-    database = "HSpector"
-    username = "hspector"
-    password = "Daniel02raptors"
+    servername = os.environ.get("servername")#"hspector"
+    database = os.environ.get("database")
+    username = os.environ.get("database_username")
+    password = os.environ.get("sql_password")
 
     server = f"{servername}.database.windows.net"
 
     driver = '{ODBC Driver 13 for SQL Server}'
 
-    #params = urllib.parse.quote_plus(f'DRIVER={driver};SERVER={server}-{password};DATABASE={database};Trusted_Connection=yes;')
     SQLALCHEMY_DATABASE_URI = f"mssql+pymssql://{username}:{password}@{server}/{database}"
 
-
-    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_SERVER = 'smtp.zoho.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'danielsplash10'  # change to other a json file later
-    MAIL_PASSWORD = 'Daniel02raptors'
+    MAIL_USERNAME = 'daniel@hspector.com'
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
