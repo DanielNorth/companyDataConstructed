0b2741a618d50b6fc70053967bbe797e9bfe49e6
Daniel <danielsplash10@gmail.com>
2021-07-22 14:46:55 -0400

    update on new company

--- companyFilling/incorprate_new_company/routes.py
+++ companyFilling/incorprate_new_company/routes.py
@@ -1,12 +1,13 @@
 from flask import render_template, url_for, flash, redirect, send_file, Blueprint, abort, request
 from flask_login import login_user, current_user, logout_user, login_required
-from companyFilling.model import DirectorResignation, Company, Director, ShareHolderStake, ShareCapital
+from companyFilling.model import UserMessage, Company, Director, ShareHolderStake, ShareCapital, User
 from companyFilling import db
 from companyFilling.changeDirector.forms import Test
 import os
 import uuid
 from datetime import date
 from .forms import CompanyInfo, SubmitButton
+import stripe
 
 incorporate_new_company = Blueprint('incorporate_new_company', __name__)
 
@@ -22,11 +23,11 @@ def new_company():
                              addressOfRegisteredOffice1=form.addressOfRegisteredOffice1.data,
                              addressOfRegisteredOffice2=form.addressOfRegisteredOffice2.data,
                              addressOfRegisteredOffice3=form.addressOfRegisteredOffice3.data,
-                             owner_id=current_user.id)
+                             owner_id=current_user.id, plan='starter')
         db.session.add(newCompany)
         db.session.commit()
 
-        newShareCaptial = ShareCapital(company_id=newCompany.id, shareClass=form.shareClass.data,
+        newShareCaptial = ShareCapital(company_id=newCompany.id, shareClass="ordinary",
                                        currency=form.currency.data, totalNumber=form.shares_issued.data,
                                        totalAmount=form.totalAmount.data, totalPaidUp=form.totalPaidUp.data)
         db.session.add(newShareCaptial)
@@ -41,16 +42,21 @@ def new_company():
 @incorporate_new_company.route("directors_founders/<company_id>/<total_shares>", methods=["POST", "GET"])
 @login_required
 def add_person(company_id, total_shares):
+    try:
+        share_holders = ShareHolderStake.query.filter_by(company_id=company_id).all()
+    except:
+        share_holders = None
 
     if request.method == "POST":
         return redirect(url_for("incorporate_new_company.review", company_id=company_id))
 
-    return render_template("newCompany/multi-step-form2.html", company_id=company_id, total_shares=total_shares)
+    return render_template("newCompany/multi-step-form2.html", company_id=company_id, total_shares=total_shares,
+                                                               share_holders=share_holders)
 
 
-@incorporate_new_company.route('add_director/<company_id>', methods=['GET', "POST"])
+@incorporate_new_company.route('add_director/<company_id>/<total_shares>', methods=['GET', "POST"])
 @login_required
-def add_director(company_id):
+def add_director(company_id, total_shares):
     company = Company.query.filter_by(id=company_id).first()
     if company.owner_id != current_user.id:
         abort(403)
@@ -76,14 +82,14 @@ def add_director(company_id):
         db.session.add(newDirector)
         db.session.commit()
 
-        return "<script>window.close();</script>"
+        return redirect(url_for('incorporate_new_company.add_person', company_id=company_id, total_shares=total_shares))
 
     return render_template("changeDirector/addDirector.html", form=form)
 
 
-@incorporate_new_company.route('add_director_corp/<company_id>', methods=['GET', "POST"])
+@incorporate_new_company.route('add_director_corp/<company_id>/<total_shares>', methods=['GET', "POST"])
 @login_required
-def add_director_corp(company_id):
+def add_director_corp(company_id, total_shares):
     company = Company.query.filter_by(id=company_id).first()
     if company.owner_id != current_user.id:
         abort(403)
@@ -106,16 +112,17 @@ def add_director_corp(company_id):
         db.session.add(newDirector)
         db.session.commit()
 
-        return """<script><script type='text/javascript'>
-    window.open('','_parent',''); 
-    window.close();
-</script>"""
+        return redirect(url_for('incorporate_new_company.add_person', company_id=company_id, total_shares=total_shares))
+
     return render_template("changeDirector/addDirectorCorp.html", form=form)
 
 
 @incorporate_new_company.route("add_existing_shareholder/<company_id>/<total_shares>", methods=["POST", "GET"])
 @login_required
 def add_existing_shareholder(company_id, total_shares):
+    company = Company.query.filter_by(id=company_id).first()
+    if company.owner_id != current_user.id:
+        abort(403)
     from companyFilling.shareHolder.forms import ShareInfo
     from companyFilling.model import ShareCapital
 
@@ -140,26 +147,27 @@ def add_existing_shareholder(company_id, total_shares):
                                                company_id=company_id, email=form.email.data)
         db.session.add(existingShareHolder)
         db.session.commit()
-        return redirect(url_for("fillingForm.edit_company_info", company_id=company_id))
+        return redirect(url_for("incorporate_new_company.add_person", company_id=company_id, total_shares=total_shares))
 
-    return render_template('shareHolder/addExistingShareHolder.html', company_id=company_id, form=form, share_classes=share_classes,
+    return render_template('newCompany/addExistingShareHolder.html', company_id=company_id, form=form, share_classes=share_classes,
                            total_shares=total_shares)
 
 
 @incorporate_new_company.route("review/<company_id>", methods=["POST", "GET"])
 @login_required
 def review(company_id):
-    #form = SubmitButton()
     company = Company.query.filter_by(id=company_id).first()
+    if company.owner_id != current_user.id:
+        abort(403)
+
     shareHolders = ShareHolderStake.query.filter_by(company_id=company_id).all()
     directors = Director.query.filter_by(company_id=company_id).all()
 
-    if request.method == "POST":
-        return redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=OfficialRickAstleyOfficialRickAstleyOfficialArtistChannel")
-
+    # if request.method == "POST":
+    #     return redirect(url_for('incorporate_new_company.checkout'))
 
     return render_template('newCompany/review.html', company=company, share_holders=shareHolders,
-                           directors=directors, company_id=company_id)
+                           directors=directors, company_id=company_id, key=os.environ.get("stripe_public_key"))
 
 
 @incorporate_new_company.route("edit/<shareHolder_id>/<company_id>", methods=["POST", "GET"])
@@ -190,9 +198,13 @@ def edit_shareholder(shareHolder_id, company_id):
     return render_template("shareHolder/editShareHolder.html", id=shareHolder_id, form=form, share_classes=share_classes)
 
 
-@incorporate_new_company.route("director/editInfo/<director_id>0", methods=["POST", 'GET'])
+@incorporate_new_company.route("director/editInfo/<director_id>", methods=["POST", 'GET'])
 @login_required
 def edit_director(director_id):
+    director = Director.query.filter_by(id=director_id).first()
+    if director.companyOwnerID != current_user.id:
+        abort(403)
+
     from companyFilling.changeDirector.forms import DirectorInfo
 
     director = Director.query.filter_by(id=director_id).first()
@@ -234,6 +246,105 @@ def edit_director(director_id):
             director.address3 = form.address3.data
 
             db.session.commit()
-            return redirect(url_for("fillingForm.edit_company_info", company_id=company_id))
+            return redirect(url_for("incorporate_new_company.review"))
 
         return render_template("changeDirector/addDirectorCorp.html", form=form)
+
+
+@incorporate_new_company.route("checkout")
+@login_required
+def checkout():
+    import stripe
+    secret_key = os.environ.get("stripe_secret_key")
+    stripe.api_key = secret_key
+
+    session = stripe.checkout.Session.create(
+        payment_method_types=['card'],
+        line_items=[{
+            'price': 'price_1JFNngBV8igYyrszd4X24ogc',
+            'quantity': 1,
+        }],
+        mode='subscription',
+        success_url=url_for("incorporate_new_company.success_payment", _external=True),
+        cancel_url=url_for("incorporate_new_company.payment_fail", _external=True)
+    )
+    return {
+        'checkout_session_id': session['id'],
+        'checkout_public_key': "pk_live_51InDYVBV8igYyrsz8ZRAHjyDQfnnSvS1AVv4BTN6iHdTw41pWOXw86yGxzEXBiKcVLVh3uZWrOPA6qQtpssY11HG00jReZIM6z"
+    }
+
+
+@incorporate_new_company.route('/stripe_webhook', methods=['POST'])
+@login_required
+def stripe_webhook():
+    if request.content_length > 1024 * 1024:
+        print('REQUEST TOO BIG')
+        abort(400)
+    payload = request.get_data()
+    sig_header = request.environ.get('HTTP_STRIPE_SIGNATURE')
+    endpoint_secret = os.environ.get("stripe_endpoint_secret")
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, endpoint_secret
+        )
+    except ValueError as e:
+        # Invalid payload
+        print('INVALID PAYLOAD')
+        return {}, 400
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        print('INVALID SIGNATURE')
+        return {}, 400
+
+    # Handle the checkout.session.completed event
+    if event['type'] == 'checkout.session.completed':
+        session = event['data']['object']
+        print(session)
+
+    return {}
+
+
+@incorporate_new_company.route("success_payment")
+@login_required
+def success_payment():
+    from flask_mail import Message
+    from companyFilling import mail
+
+    msg = Message(f'New company registration request, user_id: {current_user.id}',
+                  sender='daniel@hspector.com',
+                  recipients=['asz24687@gmail.com'])
+    msg.body = ""
+    mail.send(msg)
+
+    newMessage = UserMessage(user_id=current_user.id, message="Your request to file company is already is processed")
+    db.session.add(newMessage)
+    db.session.commit()
+
+    return render_template('newCompany/success_payment.html')
+
+
+@incorporate_new_company.route("payment_fail")
+@login_required
+def payment_fail():
+    return render_template("newCompany/fail_payment.html")
+
+
+@incorporate_new_company.route("new_shareHolder/<company_id>", methods=["POST", "GET"])
+@login_required
+def insert_shareHolder(company_id):
+    from flask import jsonify
+
+    if request.method == 'POST':
+        name = request.form['name']
+        address = request.form['address']
+        email = request.form['email']
+        totalShares = request.form['totalShares']
+
+        newShareHolder = ShareHolderStake(name=name, address=address,
+                                          email=email, shareClass="ordinary",
+                                          totalShares=int(totalShares), company_id=int(company_id))
+        db.session.add(newShareHolder)
+        db.session.commit()
+    return jsonify("success")
